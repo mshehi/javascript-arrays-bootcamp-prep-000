067cbb3549967762ae7d4d3d0b6aaed4c96121c8
Megi Shehi <mshehi97@gmail.com>
2018-06-25 00:14:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,9 +23,10 @@ function accessElementInArray(arr, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(arr){
-  
+  arr.shift();
+  return arr;
 }
 
 function removeElementFromBeginningOfArray(arr){
-  
+  return arr.slice(1);
 }
